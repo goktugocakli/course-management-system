6afd431a569cff15b1a59f3be162e4852d8dd17d
b21946366 <b21946366@cs.hacettepe.edu.tr>
2023-05-03 10:34:14 +0300

    enroll container added TODO: add functionalities

--- src/components/enrollcourse/styles/enrollcourse.js
+++ src/components/enrollcourse/styles/enrollcourse.js
@@ -9,7 +9,6 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: space-between;
     padding: 2rem;
 
     height: 100vh;
