11d586c1738682e3bafeb923d07305c87e894165
b21946366 <b21946366@cs.hacettepe.edu.tr>
2023-05-05 16:31:52 +0300

    see eva res page added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
 export { default as HomePage} from './homepage';
 export { default as SignInOut} from './signinout';
 export {default as CreateEvaluationPage} from './createevaluationpage';
+export {default as EnrollPage} from './enrollpage';
+export {default as SeeEvaResPage} from './seeevarespage';
